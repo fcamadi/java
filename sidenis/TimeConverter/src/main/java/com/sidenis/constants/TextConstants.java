84c193064f7db774e8e4437dff8af3a653c88e4f
fcamadi <30373168+fcamadi@users.noreply.github.com>
2017-07-24 22:57:16 +0200

    Add files via upload

---
+++ sidenis/TimeConverter/src/main/java/com/sidenis/constants/TextConstants.java
@@ -0,0 +1,11 @@
+package com.sidenis.constants;
+
+public class TextConstants {
+
+	public static final String TIME_NOT_VALID = "Time not valid";
+	
+	public static final String FORMAT_KO = "Format not ok";
+	public static final String REGEXP_KO = "RegExp not ok";
+	public static final String CLOCK_NOT_VALID = "Type of clock not valid!!!";
+	
+}
